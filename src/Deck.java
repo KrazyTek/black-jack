ccccff723499192bc430350a796af6ec4db2afcd
matt <mateoavramenko@gmail.com>
2025-06-10 11:44:49 -0700

    Source folder

--- src/Deck.java
+++ src/Deck.java
@@ -1,5 +1,5 @@
 public class Deck {
     private String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     private String[] suits = {"Spades", "Hearts", "Clubs", "Diamonds"};
-    // just testing smth
+
 }
